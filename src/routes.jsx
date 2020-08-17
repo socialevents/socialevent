48a072f842740ccd4c5f95c8ca7daa582d388fb5
N-Smitty <66090215+N-Smitty@users.noreply.github.com>
2020-08-17 16:51:46 -0600

    Profile page views

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import {Switch, Route} from 'react-router-dom';
-import App from '../src/app/layout/App';
-import EventDashboard from './features/events/EventDashboard/EventDashboard';
-
-
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import App from "../src/app/layout/App";
+import EventDashboard from "./features/events/EventDashboard/EventDashboard";
+import BasicInfo from "./features/nav/Profile/Settings/BasicInfo";
+import AboutMe from "./features/nav/Profile/Settings/AboutMe";
+import MyPhotos from "./features/nav/Profile/Settings/MyPhotos";
 
 export default (
   <Switch>
-    <Route exact path='/' component={EventDashboard}/>
-    <Route path='/event/:eventid' component={Event} />
-    <Route path='/createevent' component={CreateEvent}/>
+    <Route exact path="/" component={EventDashboard} />
+    <Route path="/event/:eventid" component={Event} />
+    {/* <Route path='/createevent' component={CreateEvent}/>
+    <Route path='/profile' component={Profile} /> */}
+    <Route path='/settings/aboutme' component = {AboutMe} />
+    <Route path="/settings/basicinfo" component={BasicInfo} />
+    <Route path='/settings/myphotos' component={MyPhotos}/>
   </Switch>
-)
+);
