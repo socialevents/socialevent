170f8f990623c9692bcfad121868e6fe8ea6c7d5
akash rattan <akashrattan43@Gmail.com>
2020-08-17 15:44:16 -0700

    addedevent

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,17 @@
+import React   from 'react';
+import { Container } from 'semantic-ui-react'
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar/NavBar'
+
+function App() {
+  return (
+    <div>
+      <NavBar/>
+      <Container className="main">
+      <EventDashboard/>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
