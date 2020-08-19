e82778e0fee9c77c98b5dc1076ea62ba65605866
Tourtle21 <kardlargin@gmail.com>
2020-08-19 10:04:51 -0800

    Added simple firebase button

--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -4,9 +4,39 @@ import { Menu, Container, Button } from "semantic-ui-react";
 import {Link} from 'react-router-dom';
 import "semantic-ui-css/semantic.min.css";
 import { NavLink} from 'react-router-dom'
+import firebase from 'firebase';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 
+const config = {
+  apiKey: "AIzaSyCQ8ekSC_ihYteUPp5X6ZhbnUdkTiHo_Io",
+  authDomain: "socailevent.firebaseapp.com",
+  databaseURL: "https://socailevent.firebaseio.com",
+  projectId: "socailevent",
+  storageBucket: "socailevent.appspot.com",
+  messagingSenderId: "820788796021",
+  appId: "1:820788796021:web:c2e6a480ecae394db5763c",
+  measurementId: "G-Q1J5VW2WKN"
+}
+
+firebase.initializeApp(config);
 
 class NavBar extends Component {
+
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      console.log(user);
+    })
+  }
+
+  uiConfig = {
+    signInFlow: 'popup',
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccess: () => false
+    }
+  }
   render() {
     return (
       <Menu inverted fixed="top">
@@ -23,12 +53,16 @@ class NavBar extends Component {
           
           <Menu.Item as={NavLink} to='/login' name="Login" position="right">
             <Button basic inverted content="Login"  />
-           
+            <StyledFirebaseAuth
+              uiConfig={this.uiConfig}
+              firebaseAuth={firebase.auth()}
+              />
             <Button
               basic
               inverted
               content="Sign Out"
               style={{ marginLeft: "0.5em" }}
+              onClick={() => firebase.auth().signOut()}
             />
           </Menu.Item>
           <Menu.Item name="Profile" />
