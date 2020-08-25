124fc7067a6afbb86a03667bb0df6fae8ce11b73
GKAtwood <gabrielle.k85@gmail.com>
2020-08-25 12:12:33 -0600

    styling

--- src/features/user/UserDetailed/UserDetailedPage.jsx
+++ src/features/user/UserDetailed/UserDetailedPage.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { Grid, Container, Image, Segment, Form } from "semantic-ui-react";
 import {connect} from 'react-redux';
+import ImageGrid from '../../nav/Profile/upload/ImageGrid'
 import "semantic-ui-css/semantic.min.css"
 import './UserDetailedPage.css';
 
@@ -16,12 +17,13 @@ class UserDetailedPage extends Component{
         const {name, profile_pic, gender, dob, state, profile_pics, email, description} = this.props.user;
         
         return(
+            <div className='aboutme'>
         <div class='profile-container' >   
         <div class='ui center aligned segment' >
           
             
               
-              <h1>{name}</h1>
+              <h1 className='profile'>{name}</h1>
             
                 <div class='profile-photo-info'>
                 <img class="ui small circular image" src ={profile_pic}/>
@@ -32,18 +34,23 @@ class UserDetailedPage extends Component{
                     <div>Location: {state}</div>
                 </div>
                 </div>
+                <div className='inverted'>
                 <div class='ui segment'>
-                    <h3>About Me:</h3>
+                    <h3 className='about'>About Me :</h3>
+                    <div className='description'>
                     <p>{description}</p>
+                    </div>
                 </div>
-                    <h3>My Photos</h3>
-                  <Image as ='a' size='small'  src = 'https://randomuser.me/api/portraits/men/21.jpg'/>
-                  <Image as ='a' size='small'  src = 'https://randomuser.me/api/portraits/men/22.jpg'/>
-                  <Image as ='a' size='small'  src = 'https://randomuser.me/api/portraits/men/23.jpg'/>
-                  <Image as ='a' size='small'  src = 'https://randomuser.me/api/portraits/men/24.jpg'/>
+                </div>
+               
+                    <h3 className='phototitle' >My Photos</h3>
+                    
+                 <ImageGrid/>
+                  
                 </div>
             
           
+        </div>
         </div>
             
                 
